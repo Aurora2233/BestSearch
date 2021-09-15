78af70663ec8f58c5b9c432d3d8fbbc46c269105
One Aurora <1453250595@qq.com>
2021-09-15 23:14:18 +0800

    image and name

--- chrome-expand/js/popup.js
+++ chrome-expand/js/popup.js
@@ -1,5 +1,4 @@
 $(function () {
-
 	// 加载设置
 	var defaultConfig = { color: 'white' }; // 默认配置
 	chrome.storage.sync.get(defaultConfig, function (items) {
@@ -10,34 +9,14 @@ $(function () {
 	$('#test_i18n').html(chrome.i18n.getMessage("helloWorld"));
 
 	chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-		console.log(tabs[0].id);
 		chrome.tabs.sendMessage(tabs[0].id, { max: 'MAX' }, (response) => {
-			console.log(response);
-			let data = JSON.parse(response)
-			document.getElementById('Max').innerText = data.MAX
-			document.getElementById('Min').innerText = data.MIN
-
-		})
-	})
-});
-
-// 演示2种方式操作DOM
-
-// 修改背景色
-$('#update_bg_color').click(() => {
-	executeScriptToCurrentTab('document.body.style.backgroundColor="red";')
-});
-
-// 修改字体大小
-$('#update_font_size').click(() => {
-	chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-		console.log(tabs[0].id);
-		chrome.tabs.sendMessage(tabs[0].id, { max: 'MAX' }, (response) => {
-			console.log(response);
-			let data = JSON.parse(response)
-			document.getElementById('Max').innerText = data.MAX
-			document.getElementById('Min').innerText = data.MIN
-
+			let {Max,Min} = JSON.parse(response)
+			document.getElementById('MaxPrice').innerText = Max.price
+			document.getElementById('MinPrice').innerText = Min.price
+			document.querySelector('.MaxImage').src = Max.image
+			document.querySelector('.MinImage').src = Min.image
+			document.querySelector('.MaxProductName').innerText = Max.title
+			document.querySelector('.MinProductName').innerText = Min.title
 		})
 	})
 });
